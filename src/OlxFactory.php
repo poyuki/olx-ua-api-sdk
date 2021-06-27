d3209a52332acc6d094f8e71c63268757d696704
poyuk <poyuki9@gmail.com>
2021-06-27 16:40:26 +0300

    consts

--- src/OlxFactory.php
+++ src/OlxFactory.php
@@ -4,31 +4,37 @@ namespace Parhomenko\Olx;
 
 use Parhomenko\Olx\Exceptions\UnknownCountryException;
 
-class OlxFactory {
+class OlxFactory
+{
+    public const UA = 'ua';
+    public const PL = 'pl';
+    public const BG = 'bg';
+    public const RO = 'ro';
+    public const KZ = 'kz';
+    public const PT = 'pt';
 
     /**
-     * @param $country_code
+     * @param string $country_code
      * @param array $credentials
      * @param bool $update_token
      * @return Api
      * @throws UnknownCountryException
      */
-    public static function get( $country_code, array $credentials, bool $update_token = false ): Api
+    public static function create(string $country_code, array $credentials, bool $update_token = false): Api
     {
         $links = [
-            'ua' => 'https://www.olx.ua/',
-            'pl' => 'https://www.olx.pl/',
-            'bg' => 'https://www.olx.bg/',
-            'ro' => 'https://www.olx.ro/',
-            'kz' => 'https://www.olx.kz/',
-            'pt' => 'https://www.olx.pt/',
+            self::UA => 'https://www.olx.ua/',
+            self::PL => 'https://www.olx.pl/',
+            self::BG => 'https://www.olx.bg/',
+            self::RO => 'https://www.olx.ro/',
+            self::KZ => 'https://www.olx.kz/',
+            self::PT => 'https://www.olx.pt/',
         ];
 
         if (array_key_exists($country_code, $links)) {
             return new Api($links[$country_code], $credentials, $update_token);
         }
 
-        throw new UnknownCountryException( "Country does not supported" );
+        throw new UnknownCountryException("Country does not supported");
     }
-
 }
